9c065becb68dfd9de206cdc3439049825cefafc7
ncb6206 <ncb6206@gmail.com>
2025-01-31 03:52:04 +0900

    Feat : 페이지 루트 설정

---
+++ app/(afterLogin)/[studyId]/solved/page.tsx
@@ -0,0 +1,3 @@
+export default function SolvedPage() {
+  return <div></div>;
+}
---
+++ app/(beforeLogin)/social-login/[social]/page.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import { useRouter, useSearchParams } from 'next/navigation';
+
+import sessionStore from '@/app/_store/useSession';
+import userStore from '@/app/_store/userStore';
+import { useUserInfoQuery } from '@/app/_hooks/useUserInfoQuery';
+import postAccessToken from '@/app/_api/user/postAccessToken';
+import { useEffect } from 'react';
+
+const SocialLoginPage = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const member_Id = searchParams.get('memberId');
+
+  useEffect(() => {
+    const handleSocialLogin = async () => {
+      if (!member_Id) {
+        router.push('/'); // memberId가 없으면 홈으로 리다이렉트
+        return;
+      }
+
+      try {
+        // 백엔드로 API 요청 (쿠키 설정은 백엔드가 처리)
+        const { accessToken, memberId } = await postAccessToken({
+          memberData: { memberId: member_Id },
+        });
+
+        if (accessToken) {
+          sessionStore.setState({ access_token: accessToken, memberId });
+        }
+
+        const token = accessToken || '';
+
+        if (!token) {
+          router.push('/');
+        }
+
+        router.push('/');
+
+        // 3. 사용자 정보 가져오기
+        // const { UserInfoData } = useUserInfoQuery(memberId);
+
+        // if (!!UserInfoData?.userId) {
+        //   userStore.setState({
+        //     ...UserInfoData,
+        //   });
+        // }
+
+        // // 4. Study ID 확인 및 리다이렉트
+        // const studyId = UserInfoData?.studyId || '';
+        // if (studyId) {
+        //   console.log('스터디 있음');
+        //   // router.push(`/study/${studyId}/dashboard`);
+        // } else {
+        //   console.log('스터디 없음');
+        //   // router.push('/study/search');
+        // }
+      } catch (error) {
+        console.error('Error during login:', error);
+        // router.push('/'); // 에러 발생 시 홈으로 리다이렉트
+      }
+    };
+
+    handleSocialLogin();
+  }, []);
+
+  return <></>;
+};
+
+export default SocialLoginPage;

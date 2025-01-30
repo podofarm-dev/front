ceabd31b81aa993dcef4d8b467879461a66ac880
ncb6206 <ncb6206@gmail.com>
2025-01-31 03:47:21 +0900

    Refactor : 변수 설정

--- app/_constants/path.ts
+++ app/_constants/path.ts
@@ -1,5 +1,12 @@
 import { AXIOS_BASE_URL } from '@/app/_constants/api';
 
 export const PATH = {
+  ROOT: '/',
+  DASHBOARD: '/dashboard',
   LOGIN: `${AXIOS_BASE_URL}/dev-login`,
+  STUDY_SOLVED: (studyId: string) => `/${studyId}/solved`,
+  STUDY_SOLVED_DETAIL: (studyId: string, problemId: string) => `/${studyId}/solved/${problemId}`,
+  STUDY_DASHBOARD: (studyId: string) => `/${studyId}/dashboard`,
+  STUDY_PROBLEM_LIST: (studyId: string) => `/${studyId}/problem-list`,
+  SETTING: (studyId: string) => `/${studyId}/setting`,
 } as const;

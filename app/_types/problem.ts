36ee2c82ec0f008995f6038e8e45b3de74e60b3d
ncb6206 <ncb6206@gmail.com>
2025-02-10 20:45:32 +0900

    Type : 타입 수정

--- app/_types/problem.ts
+++ app/_types/problem.ts
@@ -1,3 +1,25 @@
 import { SOLVED_STATUS } from '@/app/_constants/status';
 
 export type StatusType = keyof typeof SOLVED_STATUS;
+
+export interface SolvedDetailData {
+  studyId: string;
+  memberId: string;
+  name: string;
+  solvedProblem: number;
+  imgUrl: string;
+  rank: number;
+}
+
+export interface SolvedData {
+  problemNo: string;
+  problemTitle: string;
+  problemLevel: string;
+  problemType: string;
+  codeSolvedDate: string;
+  codeTime: string;
+}
+
+export interface SolvedList {
+  problemList: SolvedData[];
+}
--- app/_types/study.ts
+++ app/_types/study.ts
@@ -1,4 +1,4 @@
-export interface StudyData {
+export interface StudyCreateData {
   code: string;
   name: string;
   startDate: string;
@@ -17,4 +17,30 @@ export interface StudyCreateBody {
 export interface StudyEnterBody {
   code: string;
   password: string;
+}
+
+export interface StudyMemberDetail {
+  id: string;
+  name: string;
+  googleId: string;
+  solvedCount: number;
+  isLeader: boolean;
+}
+
+export interface StudyData {
+  studyId: string;
+  studyName: string;
+  studyStart: string;
+  studyEnd: string;
+  memberCount: number;
+  memberDetails: StudyMemberDetail[];
+}
+
+export interface StudyMemberCount {
+  memberCount: number;
+}
+
+export interface StudyDays {
+  remainingDays: number;
+  elapsedDays: number;
 }

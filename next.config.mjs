769d55b37ca340c1e591eced9afd43ee52b41b56
ncb6206 <ncb6206@gmail.com>
2025-02-10 20:36:02 +0900

    Feat : s3 이미지 접근 허용

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,14 @@ const nextConfig = {
     });
     return config;
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.s3.ap-northeast-2.amazonaws.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;

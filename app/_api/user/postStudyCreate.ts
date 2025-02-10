4b9aacb550ade4b3017d7f6c47338af8d5f681b6
ncb6206 <ncb6206@gmail.com>
2025-02-10 20:44:43 +0900

    Feat : 기능 추가

--- app/_api/user/postStudyCreate.ts
+++ app/_api/user/postStudyCreate.ts
@@ -1,13 +1,13 @@
 import { axiosInstance } from '@/app/_api/axiosInstance';
 import { END_POINTS } from '@/app/_constants/api';
-import { StudyCreateBody, StudyData } from '@/app/_types/study';
+import { StudyCreateBody, StudyCreateData } from '@/app/_types/study';
 
 export interface PostStudyCreateParams {
   studyData: StudyCreateBody;
 }
 
 const postStudyCreate = async ({ studyData }: PostStudyCreateParams) => {
-  const { data } = await axiosInstance.post<StudyData>(END_POINTS.STUDY_CREATE, studyData);
+  const { data } = await axiosInstance.post<StudyCreateData>(END_POINTS.STUDY_CREATE, studyData);
 
   return data;
 };

57ec2f0bb90cb5db0e1947f2591a4630a864487f
ncb6206 <ncb6206@gmail.com>
2025-02-12 16:46:10 +0900

    Style : 공통 컴포넌트 스타일 수정

--- app/_components/common/DebouncedInput.tsx
+++ app/_components/common/DebouncedInput.tsx
@@ -1,4 +1,5 @@
 import { cn } from '@/lib/utils';
+import { Search } from 'lucide-react';
 import { useEffect, useState } from 'react';
 
 const DebouncedInput = ({
@@ -26,15 +27,18 @@ const DebouncedInput = ({
   }, [value]);
 
   return (
-    <input
-      {...props}
-      value={value}
-      onChange={(e) => setValue(e.target.value)}
-      className={cn(
-        `font-lg border-block cn rounded border border-secondary-foreground bg-transparent p-2 shadow`,
-        className,
-      )}
-    />
+    <div className="flex flex-row items-center rounded border border-secondary-foreground bg-transparent p-2 focus-within:border-accent-foreground focus-within:ring-1 focus-within:ring-ring">
+      <input
+        {...props}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className={cn(
+          `font-lg border-block cn rounded border-0 bg-transparent shadow focus:outline-none`,
+          className,
+        )}
+      />
+      <Search className="cursor-pointer" />
+    </div>
   );
 };
 

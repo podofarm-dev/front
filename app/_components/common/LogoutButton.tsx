3afdace48faba140bbbe49faaa25dcc0a54b5f9e
ncb6206 <ncb6206@gmail.com>
2025-02-15 04:07:14 +0900

    Refactor : 로그아웃 클릭시 logout 로직 실행

--- app/_components/common/LogoutButton.tsx
+++ app/_components/common/LogoutButton.tsx
@@ -4,6 +4,7 @@ import { ACCESS_TOKEN_KEY } from '@/app/_constants/api';
 import { useLogOutMutation } from '@/app/_hooks/api/useLogOutMutation';
 import { Button } from '@/components/ui/button';
 import { DropdownMenuItem } from '@/components/ui/dropdown-menu';
+import Exit from '@/app/_svg/exit.svg';
 
 interface LogoutButtonProps {
   isButton: boolean;
@@ -29,8 +30,14 @@ const LogoutButton = ({ isButton }: LogoutButtonProps) => {
   return isButton ? (
     <Button onClick={logout}>로그아웃</Button>
   ) : (
-    <DropdownMenuItem className="cursor-pointer px-10 text-lg !text-primary-foreground hover:!bg-bolder">
-      로그아웃
+    <DropdownMenuItem
+      className="cursor-pointer px-10 text-lg !text-primary-foreground hover:!bg-bolder"
+      onClick={logout}
+    >
+      <div className="flex flex-row items-center gap-2">
+        <Exit />
+        <p>로그아웃</p>
+      </div>
     </DropdownMenuItem>
   );
 };

eef13483520c7f7002071d402be1add422e40664
ncb6206 <ncb6206@gmail.com>
2025-02-20 20:26:42 +0900

    Style : 컴포넌트 스타일 변경

--- app/_components/Setting/UserSetting.tsx
+++ app/_components/Setting/UserSetting.tsx
@@ -10,6 +10,7 @@ import ImageUpload from '@/app/_components/Setting/ImageUpload';
 import DeleteButton from '@/app/_components/Setting/DeleteButton';
 import { useUsernameMutation } from '@/app/_hooks/api/useUsernameMutation';
 import { useProfileUploadMutation } from '@/app/_hooks/api/useProfileUploadMutation';
+import OutStudyButton from '@/app/_components/Setting/OutStudyButton';
 
 interface FormValues {
   name?: string;
@@ -89,6 +90,14 @@ const UserSetting = ({ memberId, name, email, imgUrl }: UserSettingProps) => {
           </div>
         </div>
       </form>
+      <hr className="border-bolder" />
+      <div className="flex flex-col gap-8">
+        <span className="text-xl font-semibold">스터디 나가기</span>
+        <span className="text-base">
+          한번 계정을 삭제하면 다시 전으로 돌아갈 수 없습니다. 반드시 확인하세요.
+        </span>
+        <OutStudyButton />
+      </div>
       <hr className="border-bolder" />
       <div className="flex flex-col gap-8">
         <span className="text-xl font-semibold">계정탈퇴</span>
--- app/_components/common/TimeLine.tsx
+++ app/_components/common/TimeLine.tsx
@@ -12,8 +12,8 @@ const TimeLine = ({ time, name, title }: TimeLineProps) => {
   return (
     <div className="relative pb-6 pl-6">
       {/* 타임라인 원형 아이콘 */}
-      <span className="absolute left-[-9px] top-1 h-3 w-3 rounded-full bg-gray-400"></span>
-      <span className="absolute left-[-4px] top-2 h-full w-[0.1rem] bg-gray-400"></span>
+      <span className="absolute left-[-9px] top-1 h-3 w-3 rounded-full bg-bolder" />
+      <span className="absolute left-[-4px] top-2 h-full w-[0.1rem] bg-bolder" />
       {/* 시간 정보 */}
       <p className="mb-1 text-sm text-secondary-foreground">{convertSolvedBefore(time)} 전</p>
 

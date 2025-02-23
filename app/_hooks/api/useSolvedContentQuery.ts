b3e54e284e325bb25b8586f714bd675f09903c55
ncb6206 <ncb6206@gmail.com>
2025-02-24 03:47:35 +0900

    Feat : 푼문제 관련 로직, react query 추가

---
+++ app/_hooks/api/useSolvedContentQuery.ts
@@ -0,0 +1,14 @@
+import { AxiosError } from 'axios';
+import { useQuery } from '@tanstack/react-query';
+
+import { SolvedContent } from '@/app/_types/solved';
+import getSolvedContent from '@/app/_api/solved/getSolvedContent';
+
+export const useSolvedContentQuery = (problemId: string) => {
+  const { data: solvedContentData } = useQuery<SolvedContent, AxiosError>({
+    queryKey: ['solved-content', problemId],
+    queryFn: () => getSolvedContent({ problemId }),
+  });
+
+  return { solvedContentData };
+};

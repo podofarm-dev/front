d54706a86f403b65c551cce66560a18d9625dc35
ncb6206 <ncb6206@gmail.com>
2025-03-08 16:43:40 +0900

    Style : 스켈레톤UI bg 색깔 변경

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,9 +1,7 @@
 import { cn } from '@/lib/utils';
 
 function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return (
-    <div className={cn('animate-pulse rounded-md bg-primary-foreground', className)} {...props} />
-  );
+  return <div className={cn('animate-pulse rounded-md bg-tertiary', className)} {...props} />;
 }
 
 export { Skeleton };

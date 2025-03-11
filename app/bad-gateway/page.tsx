c17909abd2fde9853efd0180c39d86fa698505a4
ncb6206 <ncb6206@gmail.com>
2025-03-12 04:54:55 +0900

    Feat : 404, 502 페이지 추가

---
+++ app/bad-gateway/page.tsx
@@ -0,0 +1,10 @@
+const BadGateway = () => {
+  return (
+    <div className="flex h-dvh flex-col items-center justify-center">
+      <h2>502 - Bad Gateway</h2>
+      <p>서버 연결에 문제가 발생했습니다. 잠시 후 다시 시도해주세요.</p>
+    </div>
+  );
+};
+
+export default BadGateway;
---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div className="flex h-dvh flex-col items-center justify-center">
+      <h2>404 - 페이지를 찾을 수 없습니다</h2>
+      <p>요청하신 페이지가 존재하지 않습니다.</p>
+    </div>
+  );
+};
+
+export default NotFound;
